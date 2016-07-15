bae19d103af3ae9f2f81d67facab7501d508bbd0
jcsoo <jcs12311@gmail.com>
2016-07-15 15:09:58 +0800

    restruct

---
+++ example/app.js
@@ -0,0 +1,3 @@
+import framework from '../src/index';
+
+console.log('example page', framework);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-	entry: "./main.js",
+	context: path.join(__dirname, 'example'),
+	entry: "./app.js",
 	output: {
-		path: __dirname,
-		filename: "bundle.js"
+		path: path.resolve(__dirname, 'dist'),
+		filename: "./bundle.js"
 	},
 	resolve: {
-    	extensions: ['', '.js', '.jsx']
-  	},
+  		extensions: ['', '.js', '.jsx']
+	},
 	module: {
 		loaders: [
 		  /* set up jsx */
 		  {
 		    test: /\.jsx?$/,
-		    loaders: ['babel'],
-		    include: __dirname
-		  }
+		    exclude: /node_modules/,
+		    loaders: ['babel']
+		  },
+		   //less
+			{
+			    test: /\.less$/,
+			    loader: 'style!css?sourceMap!postcss!less?sourceMap'
+			},
+			//sass
+			{
+				test: /\.scss$/,
+				loader: 'style!css?sourceMap!postcss!sass?sourceMap'
+			}
 		]
 	},
+	plugins: [
+		new HtmlWebpackPlugin()
+	]
 }
